61ff3f02f93ce53a453b35a96b22e801d32be1de
SatyaJeetVS <54403898+SatyaJeetVS@users.noreply.github.com>
2025-10-21 00:13:53 +0530

    Update email host password configuration
    Remove default email host password from settings.

--- smartprofile/settings.py
+++ smartprofile/settings.py
@@ -134,7 +134,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER','satyajeet1400@gmail.com')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD','qgym vhoj gqdn igrn')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL','satyajeet1400@gmail.com')
 
 # Celery configuration
